cd4d5d6fa9e089ff71bed775bcad64c7a3f424d0
Ozho <alonsoacmil@hotmail.com>
2022-02-14 21:46:59 -0500

    Agregamos demas archivos empweb

---
+++ productos.php
@@ -0,0 +1,43 @@
+<?php include("template/encabezado.php");?>
+
+    <div class="jumbotron">
+    <h1 class="display-3"><strong>Ingreso de Productos</strong></h1>
+    <p class="lead">Productos</p>
+    <hr class="my-2">
+
+     <table class="table table-striped table-inverse table-responsive">
+        <thead class="thead-inverse">
+            <tr>
+                <th>Descripcion</th>
+                <th>Unidad Medida</th>
+                <th>Precio de Venta</th>
+                <th>Cantidad en</th>
+                <th>Cantidad en</th>
+                <th>Cantidad en</th>
+                <th>Cantidad en</th>
+                <th>Cantidad en</th>
+
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td >Producto 1</td>
+                    <td>Medida 1</td>
+                    <td>Precio 1</td>
+                    <td>Canti 1</td>
+                    <td>Canti 1</td>
+                    <td>Canti 1</td>
+                    <td>Canti 1</td>
+                    <td>Canti 1</td>
+                </tr>
+                <tr>
+                    <td scope="row">Producto 2</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+    </table>
+    
+</div>
+
+<?php include("template/pie.php");?>
