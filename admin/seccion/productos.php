11763be6d4b2f7894edeb0e46d103ec9c67d5271
unknown <alonsoacmil@hotmail.com>
2022-02-23 17:41:30 -0500

    Preparamos seccion de CRUD productos

--- admin/seccion/productos.php
+++ admin/seccion/productos.php
@@ -1,1 +1,11 @@
-Productos
+<?php include("../template/encabezado.php")?>
+
+<div class="col-md-5">
+    Formulario Agregar Productos
+</div>
+
+<div class="col-md-7">
+    Tabla de Productos (Mostrar datos de productos)
+</div>
+
+<?php include("../template/pie.php")?>
