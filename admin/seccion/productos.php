bb7c2acaaa31c5bd8b4a573302bc0391dea1c872
unknown <alonsoacmil@hotmail.com>
2022-03-02 16:49:44 -0500

    Importar datos sql

--- admin/seccion/productos.php
+++ admin/seccion/productos.php
@@ -1,46 +1,78 @@
 <?php include("../template/encabezado.php")?>
 
 <?php
-print_r($_POST);
+
+$txtId=(isset($_POST['txtId']))?$_POST['txtId']:"";
+
+$txtNombre=(isset($_POST['txtNombre']))?$_POST['txtNombre']:"";
+
+$txtCanti=(isset($_POST['txtCanti']))?$_POST['txtCanti']:"";
+
+$txtPrecioV=(isset($_POST['txtPrecioV']))?$_POST['txtPrecioV']:"";
+
+$accion=(isset($_POST['accion']))?$_POST['accion']:"";
+
+echo $txtId."<br/>";
+echo $txtNombre."<br/>";
+echo $txtCanti."<br/>";
+echo $txtPrecioV."<br/>";
+echo $accion."<br/>";
+
+switch ($accion){
+
+        case "Agregar";
+            echo "Presiona boton para Agregar";
+            break;
+
+        case "Modificar";
+            echo "Presiona boton para Agregar";
+            break;
+
+        case "Cancelar";
+            echo "Presiona boton para Agregar";
+            break;
+
+}
 
 ?>
 
 <div class="col-md-5">
-    Formulario Nuevos    Productos
+    Formulario Nuevos Productos
     <br/><br/>
     <div class="card">
         <div class="card-header">
             Datos de Producto
         </div>
+
         <div class="card-body">
            
-    <form method="POST" enctype="multipart/formdata">
-    <div class = "form-group">
-    <label for="txtId">Id Producto:</label>
-    <input type="text" class="form-control" name="txtId" id="txtId"  placeholder="Ingrese Id Producto">
-    </div>
+            <form method="POST" enctype="multipart/formdata">
 
-    <div class = "form-group">
-    <label for="txtNombre">Nombre:</label>
-    <input type="text" class="form-control" name="txtNombre" id="txtNombre"  placeholder="Ingrese Nombre Producto">
-    </div>
+        <div class = "form-group">
+        <label for="txtId">Id Producto:</label>
+        <input type="text" class="form-control" name="txtId" id="txtId"  placeholder="Ingrese Id Producto">
+        </div>
 
-    <div class = "form-group">
-    <label for="txtCanti">Cantidad:</label>
-    <input type="text" class="form-control" name="txtCanti" id="txtCanti"  placeholder="Ingrese Cantidad Producto">
-    </div>
+        <div class = "form-group">
+        <label for="txtNombre">Nombre:</label>
+        <input type="text" class="form-control" name="txtNombre" id="txtNombre"  placeholder="Ingrese Nombre Producto">
+        </div>
 
-    <div class = "form-group">
-    <label for="txtPrecioV">Precio de Venta:</label>
-    <input type="text" class="form-control" name="txtPrecioV" id="txtPrecioV"  placeholder="Ingrese Precio Venta">
-    </div>
+        <div class = "form-group">
+        <label for="txtCanti">Cantidad:</label>
+        <input type="text" class="form-control" name="txtCanti" id="txtCanti"  placeholder="Ingrese Cantidad Producto">
+        </div>
+
+        <div class = "form-group">
+        <label for="txtPrecioV">Precio de Venta:</label>
+        <input type="text" class="form-control" name="txtPrecioV" id="txtPrecioV"  placeholder="Ingrese Precio Venta">
+        </div>
     
-    <div class="btn-group" role="group" aria-label="">
-    <button type="button" name="accion" value="Agregar" class="btn btn-success">Agregar</button>
-    <button type="button" name="accion" value="Modificar" class="btn btn-warning">Modificar</button>
-    <button type="button" name="accion" value="Cancelar" class="btn btn-info">Cancelar</button>
-    
-    </div>
+            <div class="btn-group" role="group" aria-label="">
+                <button type="button" name="accion" value="Agregar" class="btn btn-success">Agregar</button>
+                <button type="button" name="accion" value="Modificar" class="btn btn-warning">Modificar</button>
+                <button type="button" name="accion" value="Cancelar" class="btn btn-info">Cancelar</button>
+            </div>
     
 
     </form>
@@ -48,15 +80,11 @@ print_r($_POST);
         </div>
         
     </div>
-
-    
-    
     
 </div>
 
 <div class="col-md-7">
-    Tabla de Productos (Mostrar datos de productos)
-
+    
 <table class="table table-bordered">
     <thead>
         <tr>
